0448783fa7c5e53bb70b489ad2d5d775ab1355b1
nikas <nikita.avdonin@gmail.com>
2016-12-01 14:23:35 +0300

    for testing in android

--- core/src/ru/electronikas/dogsexpert/neural/processing/NeuralProcessor.java
+++ core/src/ru/electronikas/dogsexpert/neural/processing/NeuralProcessor.java
@@ -4,11 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import org.encog.ml.data.MLData;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.persist.EncogDirectoryPersistence;
+import ru.electronikas.dogsexpert.neural.Breeds;
 import ru.electronikas.dogsexpert.neural.downsample.Downsample;
 import ru.electronikas.dogsexpert.neural.downsample.RGBDownsample;
 import ru.electronikas.dogsexpert.neural.image.PixmapMLData;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * Created by nikas on 11/27/16.
@@ -17,32 +22,31 @@ public class NeuralProcessor {
 
     Downsample downsample;
     BasicNetwork network;
-    int downsampleWidth = 15;
-    int downsampleHeight = 15;
+    int downsampleWidth = 50;
+    int downsampleHeight = 50;
 
     public NeuralProcessor() {
         downsample = new RGBDownsample();
-        FileHandle fh = Gdx.files.internal("data/theBestNet2_07prctErr_15x15_120-100-70.eg");
+        FileHandle fh = Gdx.files.internal("data/theBestNet0_73pErr_50x50_250-150-150-150-100_2d.eg");
         this.network = (BasicNetwork) EncogDirectoryPersistence.loadObject(fh.read());
     }
 
+    NumberFormat nf = new DecimalFormat("0.000000#");
     public void processWhatIs(Pixmap pixmap) {
-
         final PixmapMLData input = new PixmapMLData(pixmap);
         input.downsample(downsample, false, downsampleHeight,
                 downsampleWidth, 1, -1);
 
         final int winner = network.winner(input);
 
-//        System.out.println("What is: " + filename + ", it seems to be: "
-//                + this.neuron2identity.get(winner));
-
         Gdx.app.log("WINNER:", "" + winner);
-//		MLData outData = network.compute(input);
-//		for (double speed : outData.getData()) {
-//			System.out.print(nf.format(speed) + ", ");
-//		}
-//		System.out.println();
+        Gdx.app.log("WINNER:", "" + Breeds.values()[winner]);
+
+		MLData outData = network.compute(input);
+		for (double speed : outData.getData()) {
+			System.out.print(nf.format(speed) + ", ");
+		}
+		System.out.println();
 
     }
 
